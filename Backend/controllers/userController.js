28a619e4a89aea5d6d4b4e6af1161b581e67822e
Prasoon <prasoonsoni.work@gmail.com>
2022-06-20 22:13:46 +0530

    Updated headers

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -54,6 +54,12 @@ const register = async (req, res) => {
 
 const getUser = async (req, res) => {
     try {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
         const username = req.params.username
         const user = await User.findOne({ username }).select('-password').select('-messages').select('-created_at')
         if (!user) {
