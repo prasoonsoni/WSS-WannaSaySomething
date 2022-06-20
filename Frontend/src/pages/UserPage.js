ac174935925b2bfc59655447dc5eec586d76a2cd
Prasoon <prasoonsoni.work@gmail.com>
2022-06-20 21:06:20 +0530

    Issues Solved

--- Frontend/src/pages/UserPage.js
+++ Frontend/src/pages/UserPage.js
@@ -18,7 +18,9 @@ const UserPage = () => {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*"
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
                 }
             });
             const json = await response.json()
